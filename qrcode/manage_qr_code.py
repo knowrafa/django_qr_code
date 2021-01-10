642dabfeb6f23d8a948251e4d5f765d0bc2dc10c
Rafael <noprafa@gmail.dom>
2021-01-09 23:12:35 -0300

    Mudando de cv2.imread para PIL -> Image.open

--- qrcode/manage_qr_code.py
+++ qrcode/manage_qr_code.py
@@ -4,6 +4,7 @@ from django.conf import settings
 import cv2
 import numpy as np
 import os
+from PIL import Image
 
 
 class ManageQrCode:
@@ -12,7 +13,9 @@ class ManageQrCode:
 
     def __init__(self, pdf_path):
         reader = PdfFileReader(pdf_path, 'rb')
-        self.images_path = pdf_path.split(".")[0]
+        self.images_path = pdf_path.split(".pdf")[0]
+
+        # self.images_path = self.images_path.replace(" ", "_")
 
         page = self.cut_region(reader)
         path_to_pdf = self.save_cropped_pdf(page)
@@ -21,7 +24,7 @@ class ManageQrCode:
         print(self.find_qrcode(image))
 
     def cut_region(self, reader):
-
+        print(self.images_path)
         # Pegando a primeira página do pdf
         page = reader.getPage(0)
         pdf_size = page.mediaBox
@@ -39,39 +42,44 @@ class ManageQrCode:
         writer = PdfFileWriter()
         writer.addPage(page)
 
-        os.mkdir(self.images_path)
-
+        try:
+            os.mkdir(self.images_path)
+        except:
+            pass
         cropped_pdf_path = os.path.join(self.images_path, 'cropped_pdf.pdf')
         with open(cropped_pdf_path, 'wb') as cropped_pdf:
             writer.write(cropped_pdf)
         return cropped_pdf_path
 
     def pdf_to_image(self, path_to_pdf):
+        # pdf_image = convert_from_path(path_to_pdf, poppler_path=os.path.join('..', 'venv', 'poppler-0.68.0', 'bin'), dpi=1000)
+
         pdf_image = convert_from_path(path_to_pdf, poppler_path=os.path.join(settings.BASE_DIR, 'venv', 'poppler-0.68.0', 'bin'), dpi=600)
 
         # !TODO Ver como não salvar a imagem e pegar o array dela para continuar o tratamento
-        converted_image_path = os.path.join(self.images_path, 'converted_image.png')
+        file_name = "converted_image.png"
+        converted_image_path = os.path.join(self.images_path, file_name)
 
-        for image in pdf_image:
-            image.save(converted_image_path)
+        # Saving image (Optional)
+        # for image in pdf_image:
+        #     image.save(converted_image_path)
 
-        # converted_image_path = converted_image_path.split('media')[1]
-        converted_image_path = os.path.abspath(converted_image_path)
-        dir_name = os.path.split(self.images_path)[1]
-        file_name = os.path.split(converted_image_path)[1]
-        # image = cv2.imread(os.path.join('media', , converted_image_path), cv2.IMREAD_COLOR)
-        image = cv2.imread("media/" + dir_name + "/" + file_name, cv2.IMREAD_COLOR)
+        image = np.asarray(pdf_image.pop())
 
         return image
 
     def threshold(self, image):
-        # !TODO Verificar se o opencv 4.5.1 precisa de cv2.IMREAD_COLOR
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # !TODO Verificar se o
+        # opencv 4.5.1 precisa de cv2.IMREAD_COLOR
         image = cv2.UMat(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, image = cv2.threshold(image, 127, 255, cv2.THRESH_OTSU)
 
-        converted_otsu_image_path = os.path.join(self.images_path, 'converted_otsu_image.png')
-        cv2.imwrite(converted_otsu_image_path, image)
+        file_name = 'image_threshold.png'
+        image_threshold_path = os.path.join(self.images_path, file_name)
+        image4save = Image.fromarray(image.get())
+        image4save.save(image_threshold_path)
+        # cv2.imwrite(image_threshold_path, image)
 
         return image.get()
 
@@ -96,7 +104,7 @@ class ManageQrCode:
             # Voltando para UMat, pois só salva assim nessa versão
             image = cv2.UMat(image)
             # print(decoded_text)
-            qrcode_image_path = os.path.join(self.images_path, "qrcode_image.jpg")
+            qrcode_image_path = os.path.join(self.images_path, "qrcode_image.png")
             cv2.imwrite(qrcode_image_path, image)
             self.decoded_text = decoded_text
             return decoded_text
